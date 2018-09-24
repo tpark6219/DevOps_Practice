323dfd8b37d3abcb3f82c4e1f2923ab16d441163
tpark6219 <tpark6219@gmail.com>
2018-09-24 15:15:30 -0400

    First Commit

---
+++ Terraform/securitygroup.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "allow-ssh" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags {
+    Name = "allow-ssh"
+  }
+}
---
+++ Terraform/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-04681a1dbd79675a5"
+  }
+}
+
