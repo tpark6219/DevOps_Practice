c4cae878017fa93d33c2dcc06d05028f62aa1f3b
Timothy Park <tpark6219@gmail.com>
2018-10-18 15:20:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,6 @@ app = Flask(__name__)
 #cursor = conn.cursor()
 @app.route("/")
 def main():
-    return "Hello (Change 2)"
+    return "Hello (Change 3**)"
 if __name__ == "__main__":
     app.run()
