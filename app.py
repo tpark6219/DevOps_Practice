02aedbfb1b9cfe244d0cdc3b0099460c0da5e58d
Timothy Park <tpark6219@gmail.com>
2018-10-18 14:43:48 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flaskext.mysql import MySQL
+app = Flask(__name__)
+#mysql = MySQL()
+#mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
+# MySQL configurations
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = ''
+#app.config['MYSQL_DATABASE_DB'] = 'shop'
+#app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
+#mysql.init_app(app)
+#conn = mysql.connect()
+#cursor = conn.cursor()
+@app.route("/")
+def main():
+    return "Hello"
+if __name__ == "__main__":
+    app.run()
